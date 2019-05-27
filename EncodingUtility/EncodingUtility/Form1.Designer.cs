db9b37cd651b4427145a83d71c8447ff96c4af28
vaishaliyendhe <51073672+vaishaliyendhe@users.noreply.github.com>
2019-05-27 12:39:28 +0530

    Add files via upload

---
+++ EncodingUtility/EncodingUtility/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace EncodingUtility
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ColumnHeader colEncoding;
+            System.Windows.Forms.ColumnHeader colFileName;
+            System.Windows.Forms.ColumnHeader colDirectory;
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.txtFile = new System.Windows.Forms.TextBox();
+            this.lstResults = new System.Windows.Forms.ListView();
+            colEncoding = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            colFileName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            colDirectory = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(188, 27);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 0;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // txtFile
+            // 
+            this.txtFile.Location = new System.Drawing.Point(12, 30);
+            this.txtFile.Name = "txtFile";
+            this.txtFile.Size = new System.Drawing.Size(170, 20);
+            this.txtFile.TabIndex = 1;
+            // 
+            // lstResults
+            // 
+            this.lstResults.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstResults.CheckBoxes = true;
+            this.lstResults.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            colEncoding,
+            colFileName,
+            colDirectory});
+            this.lstResults.FullRowSelect = true;
+            this.lstResults.GridLines = true;
+            this.lstResults.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.lstResults.HideSelection = false;
+            this.lstResults.Location = new System.Drawing.Point(2, 108);
+            this.lstResults.Name = "lstResults";
+            this.lstResults.Size = new System.Drawing.Size(428, 222);
+            this.lstResults.TabIndex = 16;
+            this.lstResults.UseCompatibleStateImageBehavior = false;
+            this.lstResults.View = System.Windows.Forms.View.Details;
+            this.lstResults.SelectedIndexChanged += new System.EventHandler(this.lstResults_SelectedIndexChanged);
+            // 
+            // colEncoding
+            // 
+            colEncoding.Text = "Encoding";
+            colEncoding.Width = 25;
+            // 
+            // colFileName
+            // 
+            colFileName.Text = "File name";
+            colFileName.Width = 25;
+            // 
+            // colDirectory
+            // 
+            colDirectory.Text = "Directory";
+            colDirectory.Width = 25;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(701, 456);
+            this.Controls.Add(this.lstResults);
+            this.Controls.Add(this.txtFile);
+            this.Controls.Add(this.btnBrowse);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox txtFile;
+        private System.Windows.Forms.ListView lstResults;
+    }
+}
+
---
+++ EncodingUtility/EncodingUtility/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using System.IO;
+
+namespace EncodingUtility
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+           
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            dialog.InitialDirectory = "C:\\Users";
+            dialog.IsFolderPicker = true;
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                //MessageBox.Show("You selected: " + dialog.FileName);
+                File.SetAttributes(dialog.FileName, FileAttributes.Normal);
+
+                var files = Directory.GetFiles(dialog.FileName);
+
+                foreach (var file in files)
+                {
+                    string fileContent = File.ReadAllText(file, Encoding.Default);
+                    File.WriteAllText(file, fileContent, Encoding.UTF8);
+                }
+                MessageBox.Show("Done");
+            }
+        }
+
+        private void lstResults_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
